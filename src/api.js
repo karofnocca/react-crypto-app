90dbe7359ab3e79e7283ad0bf4ce495c9dfa7582
karofnocca <vKorkishenko@gmail.com>
2025-08-06 10:22:19 +0300

    hotfix api

--- src/api.js
+++ src/api.js
@@ -2,12 +2,12 @@ import { cryptoAssets, cryptoData } from "./data";
 
 export function fakeFetchCrypto() {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(cryptoData), 1);
+    setTimeout(() => resolve(cryptoData), 1000);
   });
 }
 
 export function fetchAssets() {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(cryptoAssets), 1);
+    setTimeout(() => resolve(cryptoAssets), 1000);
   });
 }
